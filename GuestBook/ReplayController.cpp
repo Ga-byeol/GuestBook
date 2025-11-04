5b84400a731ecfa5a8ab77b8f3f1d9d90a3c010b
Gabyeol <cweeds2483@gmail.com>
2025-11-04 18:39:31 +0900

    feat: 리플레이 사이클 종료 후 2초 대기 추가

--- GuestBook/ReplayController.cpp
+++ GuestBook/ReplayController.cpp
@@ -74,6 +74,7 @@ void ReplayController::StartReplay(HWND hDrawWnd, vector<Stroke> copyStroke) {
                     ReleaseDC(hDrawWnd, hdc);
                 }
                 if (r_state == ReplayState::Stopped) break; // 바깥쪽 for 루프 탈출
+                Sleep(2000);
             }
 
             // (한 사이클 재생 끝. 루프 처음으로 돌아감)
