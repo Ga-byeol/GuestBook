aa2764da7ed7a0cdb15780143c4ed43c0f687948
seeyxon <seeyxon@naver.com>
2025-11-06 13:16:00 +0900

    feat: 지우기 토글 시 이전 색상 복원 구현

--- GuestBook/DrawWindow.h
+++ GuestBook/DrawWindow.h
@@ -20,20 +20,23 @@ public:
     bool Create(HWND parentHwnd, HINSTANCE hInstance);
     HWND GetHwnd() const { return hwnd; }
     HDC GetMemDc() const; 
+    BOOL GetErasing() const { return erasing;  }
+    COLORREF GetSelectedColor() const { return selectedColor; }
     const std::vector<Stroke>& GetDrawnStrokes() const { return strokeCtrl.Strokes(); }
     void SetStrokes(std::vector<Stroke> strokes) { strokeCtrl.setStrokes(strokes); }
     void SetPenStyle(int PenNum); /// 브러쉬 컨트롤러 다이얼로그에서 받은 넘버
     void SetPenWidth(int PenWidth); /// 브러쉬 컨트롤러 다이얼로그에서 받은 두께
-/// const std::vector<Stroke>& GetDrawnStrokes() const { return strokeCtrl.Strokes(); }
+
 
     void SetToolWindow(ToolWindow* tool) { toolWindow = tool; }
     void setSelectedColor(COLORREF color) { this->selectedColor = color; }
     void setBuffer(BackBuffer& buffer) { backBuffer  = &buffer; }
-    void setErasing(bool erase) { erasing = erase; }
+    void setErasing() { erasing = !erasing; }
     void setReplaying(bool isReplaying) { this->isReplaying = isReplaying;  }
     void SetApplication(Application* a) { app = a; }
     void ClearAll();
 
+    COLORREF lastSelectedColor = RGB(0, 0, 0);
 
 private:
     LRESULT HandleMessage(UINT msg, WPARAM wParam, LPARAM lParam);
@@ -59,5 +62,5 @@ private:
     bool erasing = false;
     bool isReplaying = false;
     int currentPenStyle = PS_SOLID;
-    int currentPenWidth = 1;
+    int currentPenWidth = 5;
 };
